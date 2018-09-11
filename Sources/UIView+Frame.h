71cbdd81fd3766b021d418db69aac66c1ec5b220
luli757304397 <757304397@qq.com>
2018-09-11 09:27:36 +0800

    Initial GYMPackages

---
+++ Sources/UIView+Frame.h
@@ -0,0 +1,24 @@
+//
+//  UIView+Frame.h
+//  XianYu
+//
+//  Created by li  bo on 16/5/28.
+//  Copyright © 2016年 li  bo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface UIView (Frame)
+
+/**
+ *  返回UIView及其子类的位置和尺寸。分别为左、右边界在X轴方向上的距离，上、下边界在Y轴上的距离，View的宽和高。
+ */
+@property(nonatomic) CGFloat left;
+@property(nonatomic) CGFloat right;
+@property(nonatomic) CGFloat top;
+@property(nonatomic) CGFloat bottom;
+@property(nonatomic) CGFloat width;
+@property(nonatomic) CGFloat height;
+
+@end
